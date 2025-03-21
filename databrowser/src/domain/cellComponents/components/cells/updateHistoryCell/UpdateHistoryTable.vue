627d811dffea745c7e525b6102a921cbda1e9bac
RudiThoeni <r.thoeni@noi.bz.it>
2025-03-21 10:24:53 +0100

    view fixes

--- databrowser/src/domain/cellComponents/components/cells/updateHistoryCell/UpdateHistoryTable.vue
+++ databrowser/src/domain/cellComponents/components/cells/updateHistoryCell/UpdateHistoryTable.vue
@@ -8,22 +8,22 @@ SPDX-License-Identifier: AGPL-3.0-or-later
   <EditListTable :items="items" :hideTabLink="true" :hideSettingsColumn="true" :hideSortable="true" :hideSelectCheckbox="true" :hideTableActionHeader="true">
     <template #colGroup>
       <col class="w-32 md:w-40" />
-      <col class="w-32 md:w-80" />
       <col class="w-32 md:w-40" />
+      <col class="w-32 md:w-80" />
     </template>
 
     <template #tableHeader>
       <TableHeaderCell>Updated On</TableHeaderCell>
-      <TableHeaderCell>Update Source</TableHeaderCell>
-      <TableHeaderCell>Updated By</TableHeaderCell>      
+      <TableHeaderCell>Updated By</TableHeaderCell> 
+      <TableHeaderCell>Update Source</TableHeaderCell>     
     </template>
 
     <template #tableCols="{ item }: { item: UpdateHistoryEntry }">           
       <TableCell>
-        <EditedDateCell :date="item.LastUpdate" />
+        <EditedDateCell :date="item.LastUpdate" :format="'dd/MM/yyyy HH:mm'" />
       </TableCell>
-      <TableCell>{{ item.UpdateSource }}</TableCell>
-      <TableCell>{{ item.UpdatedBy }}</TableCell>      
+      <TableCell>{{ item.UpdatedBy }}</TableCell>  
+      <TableCell>{{ item.UpdateSource }}</TableCell>    
     </template>
     <template #noItems>No items have been defined yet</template>    
   </EditListTable>
