4597be4fc5a1fd4ba9a8bd16576f9fa4f079ea38
gappc <gapp.christian@gmail.com>
2025-04-24 11:30:42 +0200

    Fix HtmlCell styling

--- databrowser/src/domain/cellComponents/components/cells/htmlCell/HtmlCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/htmlCell/HtmlCell.vue
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <template>
-  <div v-if="isWriteable" class="html-content relative z-10 flex flex-col">
+  <div v-if="isWriteable" class="html-content">
     <TipTapEditor v-model="content" />
   </div>
   <div v-else v-html="html" class="html-content"></div>
